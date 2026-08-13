9e3ca0cff7b6a2b4b77bcb9ce3f616621a2fbba0
aditya45-coder <adityamourya76110@gmail.com>
2026-08-13 03:49:01 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountID = 123432
+let accountEmail = "aditya56@google.com"
+var accountPassword = "12345"
+accountCity = "Mumbai"
+ let accountState;
+
+ // account = 2 // to not allowed
+
+ /*
+ prefer not to use var 
+ bcz of issue block scope and functional scope
+ */
+
+
+
+accountEmail ="aditya45@gamil.com"
+accountPassword ="565443"
+accountCity ="bangluru"
+
+console.table([accountID , accountEmail , accountPassword , accountCity , accountState])
