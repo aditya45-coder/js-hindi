f5786f7092bd0ab3e81ab7157b068161facbad39
aditya45-coder <adityamourya76110@gmail.com>
2026-08-15 03:36:54 +0000

    strings

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,39 @@
+const name = "aditya"
+const repoCount = 50 
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('adit-ya-ar')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+ console.log(gameName.toUpperCase());
+ console.log(gameName.charAt(2));
+ console.log(gameName.indexOf('t'));
+ 
+const newstring = gameName.substring(0 ,4)
+console.log(newstring);
+const anotherString = gameName.slice(-6,5)
+console.log(anotherString);
+
+const newstringOne = "  aditya   "
+console.log(newstringOne);
+console.log(newstringOne.trim());
+
+const url = "https://aditya.com/hitesh%30mourya"
+
+console.log(url.replace('%30', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
+
+
+
+ 
+
+ 
